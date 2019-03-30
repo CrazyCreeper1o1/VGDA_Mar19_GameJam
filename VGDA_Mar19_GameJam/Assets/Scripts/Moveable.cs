15612d39878118ac1f060ba9e7cca1a9a34c21e1
raflos <raflos5661@gmail.com>
2019-03-29 20:58:59 -0700

    Updated movement script

--- VGDA_Mar19_GameJam/Assets/Scripts/Moveable.cs
+++ VGDA_Mar19_GameJam/Assets/Scripts/Moveable.cs
@@ -2,27 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Moveable : MonoBehaviour
 {
+    public Rigidbody2D Rigidbody;
     public float Speed { get; set; } = 10;
     bool left = false;
-    enum States { idle, walking, attacking, flinching, dying }
-    private States currentState;
+    enum States { idle, walking, jumping, flinching, dying }
+    private States currentState = States.idle;
 
     void Start()
     {
 
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         switch (currentState)
         {
             case States.walking:
-
+                Rigidbody.AddForce(new Vector2(left ? -1 : 1, 0) * Speed);
                 break;
 
         }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentState = States.walking;
+            if (!left) left = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentState = States.walking;
+            if (left) left = false;
+        }
+        else
+        {
+            currentState = States.idle;
+        }
+
     }
 }
