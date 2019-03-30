e6a5244d51b1c2d48a06269ee530113aa15949f5
TymeeK <38594702+TymeeK@users.noreply.github.com>
2019-03-30 12:03:13 -0700

    Added a ui script

---
+++ VGDA_Mar19_GameJam/Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private GameObject player;
+    public int hp;
+    public Text healthText; 
+
+    private void Awake()
+    {
+
+        player = GameObject.Find("Player");
+        hp = player.GetComponent<PlayerBehaviour> ().HP;
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "HEALTH: " + hp;
+
+    }
+}
