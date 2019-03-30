ede95fd98d36e9adf0b294bbb3e1e6284b85e49e
raflos <raflos5661@gmail.com>
2019-03-30 11:11:04 -0700

    Flinching in progress

--- VGDA_Mar19_GameJam/Assets/Scripts/Enemy/EnemyMovement.cs
+++ VGDA_Mar19_GameJam/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -5,26 +5,25 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public Rigidbody2D EnemyRigidbody;
-    public Transform target;
     public float speed = 1;
-    private GameObject player;
     private GameObject protectedCore;
     public float speedMultiplier = 1f;
+
+    private Transform target;
 
     private void Awake()
     {
         EnemyRigidbody = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Player");
+        target = GetComponent<Enemy>().target;
     }
 
     void Start()
     {
-        target = player.transform;
     }
 
     void Update()
     {
-        float angle = MathExtra.Vector2ToDiamondAngle(transform.position.y - target.transform.position.y, transform.position.x - target.transform.position.x);
+        float angle = MathExtra.Vector2ToDiamondAngle(transform.position.y - target.position.y, transform.position.x - target.position.x);
 
         Vector2 movement = MathExtra.DiamondAngleToVector2(angle);
 
@@ -36,12 +35,5 @@ public class EnemyMovement : MonoBehaviour
         float newX = x == null ? EnemyRigidbody.velocity.x : (float)x;
         float newY = y == null ? EnemyRigidbody.velocity.y : (float)y;
         EnemyRigidbody.velocity = new Vector2(newX, newY);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject == player)
-            // to-do damage amount variable
-            player.GetComponent<PlayerBehaviour>().TakeDamage(1);
     }
 }
