f9be6049ef7c5e01bb73fc3180e0db659c521328
raflos <raflos5661@gmail.com>
2019-03-30 15:41:51 -0700

    Giant waste of time

--- VGDA_Mar19_GameJam/Assets/Scripts/Stage.cs
+++ VGDA_Mar19_GameJam/Assets/Scripts/Stage.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemy1Prefab, enemy2Prefab;
+    public GameObject itemPrefab;
+
+    private int enemySpawnTimer = 0;
+    private int itemSpawnTimer = 0;
+
     void Start()
     {
-        
+        enemySpawnTimer = Random.Range(0, 500);
+        itemSpawnTimer = Random.Range(0, 500);
+        enemy1Prefab.GetComponent<Enemy>().target = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (enemySpawnTimer > 0)
+            enemySpawnTimer--;
+        else
+        {
+            AddEnemy();
+            enemySpawnTimer = Random.Range(0, 500);
+        }
+
+        if (itemSpawnTimer > 0)
+            itemSpawnTimer--;
+        else
+        {
+            AddItem();
+            itemSpawnTimer = Random.Range(0, 500);
+        }
+    }
+
+    void AddEnemy()
+    {
+        GameObject enemy = Instantiate(enemy1Prefab);
+        enemy.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-30, 60));
+        enemy.transform.parent = transform;
+    }
+
+    void AddItem()
+    {
+        GameObject item = Instantiate(itemPrefab);
+        item.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(0, 500));
+        item.transform.parent = transform;
     }
 }
