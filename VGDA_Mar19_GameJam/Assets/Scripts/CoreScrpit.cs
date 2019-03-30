6009b554ffd196e04ee0406a13e68ea3ca2dd4a6
Angel Rubio <>
2019-03-29 22:37:33 -0700

    Created a file that gives the enemy objective an hp bar and the enemy can damage it

---
+++ VGDA_Mar19_GameJam/Assets/Scripts/CoreScrpit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreScrpit : MonoBehaviour
+{
+    
+    public int hp = 100;
+    bool flinching = False;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Damage(int dmg)
+    {
+        if (!flinching)
+            hp -= dmg:
+    }
+}
