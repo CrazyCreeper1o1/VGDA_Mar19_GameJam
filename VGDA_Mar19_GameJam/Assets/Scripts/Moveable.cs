b8e03f3578e029381737aed89cec507a4d67bb2a
raflos <raflos5661@gmail.com>
2019-03-29 19:54:36 -0700

    Added Moveable Script for moving objects

---
+++ VGDA_Mar19_GameJam/Assets/Scripts/Moveable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed { get; set; } = 10;
+    bool left = false;
+    enum States { idle, walking, attacking, flinching, dying }
+    private States currentState;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.walking:
+
+                break;
+
+        }
+    }
+}
